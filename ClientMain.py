7b7f42e7027fb92f309d345f12a8e8cb50e88a96
Brian Semrau <brian.semrau@gmail.com>
2017-06-25 20:18:49 -0400

    Image capture works with PiCamera

--- ClientMain.py
+++ ClientMain.py
@@ -1,9 +1,17 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
 
-cam = cv2.VideoCapture(0)
+cam = PiCamera()
+raw_capture = PiRGBArray(cam)
+
+time.sleep(0.1)
 
 while True:
-    ret, image = cam.read()
+    cam.capture(raw_capture, format='bgr')
+    image = raw_capture.array
     scaled = cv2.resize(image, (240, int(240. * image.shape[0] / image.shape[1])))
     # send image over TCP
-    cv2.imshow("Videocapture", scaled)
+    cv2.imshow("test", scaled)
+    cv2.waitKey(0)

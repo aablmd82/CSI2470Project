0ce4b1190133153f2fe6db9151402ea4021af2bd
Brian Semrau <brian.semrau@gmail.com>
2017-12-01 16:16:23 -0500

    ClientMain now displays video capture

--- ClientMain.py
+++ ClientMain.py
@@ -6,3 +6,4 @@ while True:
     ret, image = cam.read()
     scaled = cv2.resize(image, (240, int(240. * image.shape[0] / image.shape[1])))
     # send image over TCP
+    cv2.imshow("Videocapture", scaled)

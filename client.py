4e56f1e784a6c154ffe371b893209a25f555048c
srdallas <srdallas@oakland.edu>
2017-12-01 17:11:02 -0500

    Updated client and server
    decodes and encodes data when sending now so it actually works lmao

--- client.py
+++ client.py
@@ -8,20 +8,27 @@ host = socket.gethostname()     # Get local machine name
 port = 60000                    # Reserve a port for your service.
 
 s.connect((host, port))
-s.send("Hello server!")
+s.send(("Hello server!").encode())
 
-with open('received_file', 'wb') as f:
-    print ("file opened")
-    while True:
+
+##with open('received_file', 'wb') as f:
+##    print ("file opened")
+##    while True:
+##        print("receiving data...")
+##        data = s.recv(1024)
+##        print("data =%s", (data))
+##        if not data:
+##            break
+##        # write data to a file
+##        f.write(data)
+##
+##f.close()
+##print('Successfully get the file')
+while True:
         print("receiving data...")
         data = s.recv(1024)
-        print("data=%s", (data))
+        print("data =%s", (data).decode())
         if not data:
             break
-        # write data to a file
-        f.write(data)
-
-f.close()
-print('Successfully get the file')
 s.close()
 print('connection closed')
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import socket                   # Import socket module
 port = 60000                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
+#host = "some ip" #if you want to connect to a different host
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
 
@@ -15,17 +16,17 @@ while True:
     conn, addr = s.accept()     # Establish connection with client.
     print ("Got connection from", str(addr))
     data = conn.recv(1024)
-    print('Server received', repr(data))
+    print('Server received', (data).decode())
 
-    filename='mytext.txt'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
+##    filename='image.txt'
+##    f = open(filename,'rb')
+##    l = f.read(1024)
+##    while (l):
+##       conn.send(l)
+##       print('Sent ',repr(l))
+##       l = f.read(1024)
+##    f.close()
 
     print('Done sending')
-    conn.send('Thank you for connecting')
+    conn.send(('Thank you for connecting').encode())
     conn.close()

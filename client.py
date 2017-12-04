c14f09dc3463bdbbfb85bad4b8cf39430020f306
srdallas <srdallas@oakland.edu>
2017-12-04 18:02:14 -0500

    Changed server and client
    Changed to work with other python scripts. Need to rework server side where it actually handles the data received.

--- client.py
+++ client.py
@@ -1,35 +1,85 @@
 # client.py ver 1.0.0
 # 12/1/2017 Sean Dallas
 
-import socket                   # Import socket module
+from socket import *
+from struct import pack
 
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-port = 60000                    # Reserve a port for your service.
 
-s.connect((host, port))
-s.send(("Hello server!").encode())
+class ClientProtocol:
 
-def transfer(image):
-        s.send((image).encode())
-##with open('received_file', 'wb') as f:
-##    print ("file opened")
-##    while True:
+    def __init__(self):
+        self.socket = None
+
+    def connect(self, server_ip, server_port):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((server_ip, server_port))
+
+    def close(self):
+        self.socket.shutdown(SHUT_WR)
+        self.socket.close()
+        self.socket = None
+
+    def send_image(self, image_data):
+
+        # use struct to make sure we have a consistent endianness on the length
+        length = pack('>Q', len(image_data))
+
+        # sendall to make sure it blocks if there's back-pressure on the socket
+        self.socket.sendall(length)
+        self.socket.sendall(image_data)
+
+        ack = self.socket.recv(1)
+
+        # could handle a bad ack here, but we'll assume it's fine.
+
+if __name__ == '__main__':
+    cp = ClientProtocol()
+
+    image_data = None
+    with open('IMG_0077.jpg', 'r') as fp:
+        image_data = fp.read()
+
+    assert(len(image_data))
+    cp.connect('127.0.0.1', 55555)
+    cp.send_image(image_data)
+    cp.close()
+
+
+
+
+
+
+
+
+##import socket                   # Import socket module
+##
+##s = socket.socket()             # Create a socket object
+##host = socket.gethostname()     # Get local machine name
+##port = 60000                    # Reserve a port for your service.
+##
+##s.connect((host, port))
+##s.send(("Hello server!").encode())
+##
+##def transfer(image):
+##        s.send((image).encode())
+####with open('received_file', 'wb') as f:
+####    print ("file opened")
+####    while True:
+####        print("receiving data...")
+####        data = s.recv(1024)
+####        print("data =%s", (data))
+####        if not data:
+####            break
+####        # write data to a file
+####        f.write(data)
+####
+####f.close()
+####print('Successfully get the file')
+##while True:
 ##        print("receiving data...")
 ##        data = s.recv(1024)
-##        print("data =%s", (data))
+##        print("data =%s", (data).decode())
 ##        if not data:
 ##            break
-##        # write data to a file
-##        f.write(data)
-##
-##f.close()
-##print('Successfully get the file')
-while True:
-        print("receiving data...")
-        data = s.recv(1024)
-        print("data =%s", (data).decode())
-        if not data:
-            break
-s.close()
-print('connection closed')
+##s.close()
+##print('connection closed')
--- server.py
+++ server.py
@@ -1,32 +1,87 @@
 # server.py ver 1.0.0
 # 12/1/2017 Sean Dallas
+from os import *
+from socket import *                   # Import socket module ABLEMIND
+from struct import unpack
 
-import socket                   # Import socket module
 
-port = 60000                    # Reserve a port for your service.
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-#host = "some ip" #if you want to connect to a different host
-s.bind((host, port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
+class Server:
 
-print ("Server listening....")
+    def __init__(self):
+        self.socket = None
+        self.output_dir = '.'
+        self.file_num = 1
 
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print ("Got connection from", str(addr))
-    data = conn.recv(1024)
-    print('Server received', (data).decode())
+    def listen(self, server_ip, server_port):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind((server_ip, server_port))
+        self.socket.listen(1)
 
-##    filename='image.txt'
-##    f = open(filename,'rb')
-##    l = f.read(1024)
-##    while (l):
-##       conn.send(l)
-##       print('Sent ',repr(l))
-##       l = f.read(1024)
-##    f.close()
+    def handle_images(self):
 
-    print('Done sending')
-    conn.send(('Thank you for connecting').encode())
-    conn.close()
+        try:
+            while True:
+                (connection, addr) = self.socket.accept()
+                try:
+                    bs = connection.recv(8)
+                    (length,) = unpack('>Q', bs)
+                    data = b''
+                    while len(data) < length:
+                        # doing it in batches is generally better than trying
+                        # to do it all in one go, so I believe.
+                        to_read = length - len(data)
+                        data += connection.recv(
+                            4096 if to_read > 4096 else to_read)
+
+                    # send our 0 ack
+                    #assert len(b'\00') == 1
+                    #connection.sendall(b'\00')  i don't think this assert is needed
+                finally:
+                    connection.shutdown(SHUT_WR)
+                    connection.close()
+
+                with open(os.path.join(
+                        self.output_dir, '%06d.jpg' % self.file_num), 'w'
+                ) as fp:
+                    fp.write(data)
+
+                self.file_num += 1
+        finally:
+            self.close()
+
+    def close(self):
+        self.socket.close()
+        self.socket = None
+
+
+     # could handle a bad ack here, but we'll assume it's fine.
+
+if __name__ == '__main__':
+    sp = Server()
+    sp.listen('127.0.0.1', 55555)
+    sp.handle_images()
+
+
+
+
+
+##
+##
+##port = 60000                    # Reserve a port for your service.
+##s = socket.socket()             # Create a socket object
+##host = socket.gethostname()     # Get local machine name
+##s.bind((host, port))            # Bind to the port
+##s.listen(5)                     # Now wait for client connection.
+##
+##print ("Server listening....")
+##
+##while True:
+##    conn, addr = s.accept()     # Establish connection with client.
+##    print ("Got connection from", str(addr))
+##
+##
+##    data = conn.recv(1024)
+##    print('Server received', (data).decode())
+##    print('Done sending')
+##    conn.send(('Thank you for connecting').encode())
+##    conn.close()
